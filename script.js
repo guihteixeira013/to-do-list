60f3f8f041d45edf6e9436534d95a0c6c576d131
Guilherme <guilherme.teixeira013@protonmail.com>
2024-09-22 12:34:44 -0300

    Update 1.1
    	Take the "taskInpunt" value to "newContent" list;
    	Validating if the Data is empty.

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 const addBtn = document.getElementById("addTaskBtn");
 const taskList = document.getElementById("taskList");
+let taskInput = document.getElementById("taskInput"); // Take the Input
 
 // Clicking on the "add"
 addBtn.addEventListener("click", (event) => {
+    const taskText = taskInput.value.trim(); // Take the task text and remove the extra spaces
 
-    // Adding a new list element
-    let newLi = document.createElement("li"); 
-    let newContent = document.createTextNode("test2");
-    newLi.appendChild(newContent);
-    taskList.appendChild(newLi)
+    if (taskText !== "") {
+        // Adding a new list element
+        let newLi = document.createElement("li"); 
+        let newContent = document.createTextNode(taskText);
+        newLi.appendChild(newContent);
+        taskList.appendChild(newLi)
+    } else {
+        alert("Please enter a task before adding.") // Alert if input is empty
+    }
 });
-
